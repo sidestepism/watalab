551d96690b0c3e6c0d673073574845e53145e67a
sidestepism <fushimi.ryohe.i@gmail.com>
2014-09-22 22:32:35 +0900

    べると合流

--- watalab-server.js
+++ watalab-server.js
@@ -1,16 +1,14 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(80);
+server.listen(process.env.PORT || 80);
 
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
-});
+app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+	socket.on('speech recognized', function(data) {
+		socket.broadcast.emit('recognition result', data);
+	})
 });
