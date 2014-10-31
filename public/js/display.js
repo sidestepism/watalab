8fb2585433fd7b8de877ec0135e72dc179b7afa3
sidestepism <fushimi.ryohe.i@gmail.com>
2014-10-31 13:37:21 +0900

    私→あなた

--- public/js/display.js
+++ public/js/display.js
@@ -71,8 +71,9 @@ socket.on('recognition result', function(data) {
         final = final.replace("私", "わたし");
         interim = interim.replace("私", "わたし");
 
+        final = final.replace("あなた", "わたし");
+        interim = interim.replace("あなた", "わたし");
         
-
         $("#" + dom_id + " .final").text(final);
         $("#" + dom_id + " .interim").text(interim);
     };
